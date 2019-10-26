77b1cc5a504087ab19f8a916c3fd2fca6b4319a5
mrinex <mrinex@mail.ru>
2019-10-26 18:06:28 +0300

    feat: add style

--- 4x4.js
+++ 4x4.js
@@ -1,4 +1,4 @@
-const map = [
+const map4 = [
     ["#00BCD4", "#FFEB3B","#FFEB3B","#00BCD4"],
     ["#FFEB3B", "#FFC107","#FFC107","#FFEB3B"],
     ["#FFEB3B", "#FFC107","#FFC107","#FFEB3B"],
@@ -8,8 +8,8 @@ const map = [
 const canvas = document.getElementById("myCanvas"); 
 const context = canvas.getContext("2d");
 
-width = map[0].length; 
-height = map.length;
+width = map4[0].length; 
+height = map4.length;
 var cellSize = 128;
 
 canvas.width = 4 * cellSize;
@@ -17,7 +17,7 @@ canvas.height = 4 * cellSize;
 
 for(var row = 0; row < height; row++) {
     for(var col = 0; col < width; col++) {
-        context.fillStyle = map[row][col];
+        context.fillStyle = map4[row][col];
         context.fillRect(col * cellSize, row * cellSize, cellSize, cellSize);
     }
 }
--- index.js
+++ index.js
@@ -5,6 +5,22 @@ var img = new Image();
 img.src = 'data/image.png';
 
 img.onload = function() {
-    context.drawImage(img, 0, 0);
+    context.drawImage(img, 128, 128);
 };
-var imageData = context.getImageData(0,0, 512, 512);
+
+let xi = document.getElementById('img');
+let x4 = document.getElementById('x4');
+let x32 = document.getElementById('x32');
+console.log(xi);
+
+xi.addEventListener('click', ()=>{
+    const scr = document.getElementById('i')
+    console.log(scr)
+    scr.src = 'index.js'
+})
+
+x4.addEventListener('click', ()=>{
+    const scr = document.getElementById('i')
+    console.log(scr)
+    scr.src = '4x4.js'
+})
